85efa25b58da872331b999ce794cb7a005c7a806
Gabriela Cuello <gab.jo.cuello@gmail.com>
2019-10-04 10:07:03 -0500

    initial commit | created classes and event listener

---
+++ main-01.js
@@ -0,0 +1,37 @@
+// wrapping the main.js code in this event listner will ensure that the program will not run until
+// document is finished loading
+window.addEventListener("load", function(event) {
+
+    "use strict";
+
+      //// FUNCTIONS ////
+    
+  
+    var render = function() {
+  
+      display.renderColor(game.color);
+      display.render();
+  
+    };
+  
+    var update = function() {
+  
+      game.update();
+  
+    };
+
+
+
+
+
+
+
+var controller = new Controller();
+
+var display = new Display(document.querySelector('canvas'));
+
+var game = new Game();
+
+var engine = new Engine (1000/30, render, update);
+
+});
